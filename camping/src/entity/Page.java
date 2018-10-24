949a030bce6bf99c95214f71164dcb0f8ddf1bfa
vszealot <vszealot@gmail.com>
2018-10-24 13:10:49 +0900

    페이징전 임시저장

--- camping/src/entity/Page.java
+++ camping/src/entity/Page.java
@@ -1,77 +1,52 @@
 package entity;
 
 public class Page {
-	private int totalRecord = 0; 	// 전체 게시물 갯수
-	private int numPerPage = 0; 	// 한페이지에 보여지는 게시물 갯수
-	private int pagePerBlock = 0;	// 페이지의 한 블럭
-	private int totalPage = 0; 		// 전체 페이지
-	private int totalBlock = 0; 	// 전체 블럭
-	private Integer nowPage = 0; 	// 현재 페이지
-	private Integer nowBlock = 0; 	// 현재 블럭
-	private int beginPerPage = 0; 	// 한페이지의 맨 위 게시물
+	private int page = 1;
+	private int perPageNum = 10;
+	private int rowStart;
+	private int rowEnd;
 
-	public int getTotalRecord() {
-		return totalRecord;
+	public void setPage(int page) {
+		if (page <= 0) {
+			this.page = 1;
+			return;
+		}
+		this.page = page;
 	}
 
-	public void setTotalRecord(int totalRecord) {
-		this.totalRecord = totalRecord;
+	public int getPage() {
+		return page;
 	}
 
-	public int getNumPerPage() {
-		return numPerPage;
+	public void setPerPageNum(int perPageNum) {
+		if (perPageNum <= 0 || perPageNum > 100) {
+			this.perPageNum = 10;
+			return;
+		}
+		this.perPageNum = perPageNum;
 	}
 
-	public void setNumPerPage(int numPerPage) {
-		this.numPerPage = numPerPage;
+	public int getPerPageNum() {
+		return this.perPageNum;
 	}
 
-	public int getPagePerBlock() {
-		return pagePerBlock;
+	public int getPageStart() {
+		return (this.page - 1) * perPageNum;
 	}
 
-	public void setPagePerBlock(int pagePerBlock) {
-		this.pagePerBlock = pagePerBlock;
+	@Override
+	public String toString() {
+		return "Criteria [page=" + page + ", perPageNum=" + perPageNum + "" + ", rowStart=" + getRowStart()
+				+ ", rowEnd=" + getRowEnd() + "]";
 	}
 
-	public int getTotalPage() {
-		return totalPage;
+	public int getRowStart() {
+		rowStart = ((page - 1) * perPageNum) + 1;
+		return rowStart;
 	}
 
-	public void setTotalPage(int totalPage) {
-		this.totalPage = totalPage;
+	public int getRowEnd() {
+		rowEnd = rowStart + perPageNum - 1;
+		return rowEnd;
 	}
-
-	public int getTotalBlock() {
-		return totalBlock;
-	}
-
-	public void setTotalBlock(int totalBlock) {
-		this.totalBlock = totalBlock;
-	}
-
-	public Integer getNowPage() {
-		return nowPage;
-	}
-
-	public void setNowPage(Integer nowPage) {
-		this.nowPage = nowPage;
-	}
-
-	public Integer getNowBlock() {
-		return nowBlock;
-	}
-
-	public void setNowBlock(Integer nowBlock) {
-		this.nowBlock = nowBlock;
-	}
-
-	public int getBeginPerPage() {
-		return beginPerPage;
-	}
-
-	public void setBeginPerPage(int beginPerPage) {
-		this.beginPerPage = beginPerPage;
-	}
-
 }
--- camping/web/campingdb.sql
+++ camping/web/campingdb.sql
@@ -1,2 +1,7 @@
 select * from tab;
 select * from CAMPING_TEST;
+select rownum, * from CAMPING_TEST where rownum>=11 and rownum<=20;
+
+select * from (select rownum rn, aa.* from (select *from CAMPING_TEST)aa)where rn>=1 and rn<=10;
+
+select * from (select rownum rn, aa.* from (select *from CAMPING_TEST where addr1 like '%ÃæÃ»%')aa)where rn>=1 and rn<=10;
